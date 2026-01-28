003d139941871ce77f745e6138df7f6b10e46a6e
Marco Barozzi <mbzstudent@gmail.com>
2026-01-28 17:41:53 -0300

    onboarding screen - page indicator

--- lib/views/onboarding/onboarding_screen.dart
+++ lib/views/onboarding/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rental_application/core/constants/color_constants.dart';
 import 'package:rental_application/models/onboarding_content.dart';
 import 'package:rental_application/views/onboarding/widgets/Onboarding_page.dart';
 
@@ -45,6 +47,34 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             itemBuilder: (context, index) {
               return OnboardingPage(content: _contents[index]);
             },
+          ),
+          Positioned(
+            bottom: 60.h,
+            left: 0,
+            right: 0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+
+              children: [
+                Row(
+                  children: List.generate(
+                    _contents.length,
+                    (index) => AnimatedContainer(
+                      duration: const Duration(milliseconds: 300),
+                      margin: EdgeInsets.symmetric(horizontal: 4.w),
+                      height: 8.h,
+                      width: _currentPage == index ? 24.w : 8.w,
+                      decoration: BoxDecoration(
+                        color: _currentPage == index
+                            ? AppColors.surface
+                            : AppColors.surface.withValues(alpha: 0.5),
+                        borderRadius: BorderRadius.circular(4.r),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
