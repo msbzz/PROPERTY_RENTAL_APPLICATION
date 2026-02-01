cee89c61e72a3f2d959e490e002a475e517ec4dc
Marco Barozzi <mbzstudent@gmail.com>
2026-02-01 13:30:52 -0300

    tela de locação (tenant screen) part4

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rental_application/core/constants/color_constants.dart';
 import 'package:rental_application/core/constants/text_constants.dart';
 import 'package:rental_application/views/home/widgets/home_app_bar.dart';
 
@@ -22,10 +23,39 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Padding(
                 padding: EdgeInsetsGeometry.all(16.w),
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       AppTexts.findYourHome,
-                      style: TextStyle(fontSize: 24.sp),
+                      style: TextStyle(
+                        fontSize: 24.sp,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.textPrimary,
+                      ),
+                    ),
+                    SizedBox(height: 16.h),
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        padding: EdgeInsets.all(12.w),
+                        decoration: BoxDecoration(
+                          color: AppColors.surface,
+                          borderRadius: BorderRadius.circular(16.r),
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(Icons.search, color: AppColors.textSecondary),
+                            SizedBox(width: 12.w),
+                            Text(
+                              AppTexts.searchHint,
+                              style: TextStyle(
+                                color: AppColors.textSecondary,
+                                fontSize: 16.sp,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ],
                 ),
