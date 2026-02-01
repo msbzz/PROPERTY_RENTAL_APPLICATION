33d96923506509baf32ea6ba574ddee06ab6b71f
Marco Barozzi <mbzstudent@gmail.com>
2026-02-01 13:18:31 -0300

    tela de locação (tenant screen) part3

--- lib/views/home/home_screen.dart
+++ lib/views/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rental_application/core/constants/text_constants.dart';
 import 'package:rental_application/views/home/widgets/home_app_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,7 +14,26 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: CustomScrollView(slivers: [HomeAppBar()])),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            HomeAppBar(),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsetsGeometry.all(16.w),
+                child: Column(
+                  children: [
+                    Text(
+                      AppTexts.findYourHome,
+                      style: TextStyle(fontSize: 24.sp),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
