48d7ce718d1d75ab1e9cc2a128a86f56f0d3b7ba
Marco Barozzi <mbzstudent@gmail.com>
2026-02-03 06:10:25 -0300

    tela de favoritos (favorites screen) part1

--- lib/views/favorites/favorites_screen.dart
+++ lib/views/favorites/favorites_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rental_application/core/common/widgets/page_layout.dart';
+import 'package:rental_application/core/constants/color_constants.dart';
+import 'package:rental_application/models/proterty_model.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Favorites Screen')));
+    return PageLayout(
+      title: 'Favorites Screen',
+      body: Property.dummyProperties.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.favorite_outline,
+                    size: 64.sp,
+                    color: AppColors.textSecondary,
+                  ),
+                  SizedBox(height: 16.h),
+                  Text('No Favorites yet'),
+                ],
+              ),
+            )
+          : GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisExtent: 16.h,
+                crossAxisSpacing: 16.h,
+                childAspectRatio: 0.65,
+              ),
+              itemBuilder: (context, index) {},
+            ),
+      //actions: [],
+    );
   }
 }
